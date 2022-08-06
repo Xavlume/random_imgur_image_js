65eaa14d999e2b47a0afdf0641b4dfb449a88472
Xavlume <xavlumme@gmail.com>
2022-08-06 14:21:29 -0400

    first commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# Random Imgur Image Js
+
+This website uses frontend javascript to fetch a random imgur image and html to display it.
+
+    
---
+++ index.js
@@ -0,0 +1,32 @@
+// Edited from https://stackoverflow.com/a/1349426
+function get_random_imgur_link(){
+    var result = 'https://i.imgur.com/';
+    var characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz01234567890123456789';
+    var charactersLength = characters.length;
+    for ( var i = 0; i < 5; i++ ) {
+      result += characters.charAt(Math.floor(Math.random() * 
+ charactersLength));
+   }
+   return result + ".png";
+}
+
+let is_redirect = false
+
+async function check_if_redirect(file) {
+  let myObject = await fetch(file);
+  is_redirect = myObject.redirected;
+}
+
+async function display_random_imgur_image() {
+  var the_link = get_random_imgur_link();
+  await check_if_redirect(the_link)
+  if (is_redirect){
+    display_random_imgur_image()
+  }
+  else{
+    document.getElementById("the-img").src = the_link;
+    document.title = the_link;
+  }
+}
+
+document.onclick = display_random_imgur_image
